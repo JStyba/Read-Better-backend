03edccf7b196be3cad34e4a2023e43828d8a0cc7
Jarek Styba <jarek.styba@gmail.com>
2018-10-11 15:38:49 +0200

    cleaned up entry service

--- src/main/java/com/readbetter/main/service/EntryService.java
+++ src/main/java/com/readbetter/main/service/EntryService.java
@@ -20,58 +20,15 @@ import java.util.Optional;
 public class EntryService implements IEntryService {
 
 
-    //    @Override
-//    public Entry findWordDefinition(String searchedWord) throws IOException {
-//
-//        URL url = new URL("https://od-api.oxforddictionaries.com:443/api/v1/inflections/en/" + searchedWord);
-//        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-//        conn.setRequestMethod("GET");
-//        conn.setRequestProperty("Accept", "application/json");
-//        conn.setRequestProperty("app_id", "3c699787");
-//        conn.setRequestProperty("app_key", "4a30da605e25de91cf692d808bdb069d");
-//
-//        if (conn.getResponseCode() != 200) {
-//            throw new RuntimeException("Failed : HTTP error code : "
-//                    + conn.getResponseCode());
-//        }
-//
-//
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//        StringBuilder stringBuilder = new StringBuilder();
-//
-//        String line = null;
-//        while ((line = reader.readLine()) != null) {
-//            stringBuilder.append(line + "\n");
-//        }
-//
-//        String wordEntry = stringBuilder.toString();
-//        ObjectMapper mapper = new ObjectMapper();
-//        final JsonNode arrNode = mapper.readTree(wordEntry).path("results");
-//        System.out.println(arrNode.toString());
-//        if (arrNode.isArray()) {
-//            for (final JsonNode objNode : arrNode) {
-////                System.out.println(objNode.get("id"));
-//            }
-//        }
-//
-//        String entryString = mapper.writeValueAsString(arrNode);
-////        System.out.println(entryString);
-//
-//        String splittedEntryString = entryString.substring(entryString.indexOf("definitions"), entryString.indexOf("\"]", entryString.indexOf("definitions"))).replaceFirst("definitions", "");
-//        String splittedEntryString2 = entryString.substring(entryString.indexOf("etymologies"), entryString.indexOf("\"]", entryString.indexOf("etymologies"))).replaceFirst("etymologies", "");
-//        return new Entry(splittedEntryString.replaceAll("[^,&&\\W&&\\S]", ""));
-////            return "DEFINITION OF " + searchedWord.toUpperCase() + ": " + splittedEntryString.replaceAll("[^,&&\\W&&\\S]", "") + ".";
-////            System.out.println("ETYMOLOGY OF " + searchedWord.toUpperCase() + ": " + splittedEntryString2.replaceAll("[^,&&\\W&&\\S]", "") + ".");
-//
-//    }
+
     @Override
     public JsonNode getDictionaryJson(String wordToLookUp) throws IOException {
         URL url = new URL("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" + wordToLookUp + "/definitions");
         HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Accept", "application/json");
-        conn.setRequestProperty("app_id", "xxxxxxxx");
-        conn.setRequestProperty("app_key", "xxxxxxxxxxxxxxxxxxxxxxxxx");
+        conn.setRequestProperty("app_id", "3c699787");
+        conn.setRequestProperty("app_key", "4a30da605e25de91cf692d808bdb069d");
 
         if (conn.getResponseCode() != 200) {
             throw new RuntimeException("Failed : HTTP error code : "
