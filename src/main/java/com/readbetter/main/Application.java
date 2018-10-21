c060de4fd7ab0a86d2e743f53a6c1a69ce96d4f3
Jarek Styba <jarek.styba@gmail.com>
2018-10-21 14:36:33 +0200

    pom.xml update

--- src/main/java/com/readbetter/main/Application.java
+++ src/main/java/com/readbetter/main/Application.java
@@ -2,6 +2,7 @@ package com.readbetter.main;
 
 import com.readbetter.main.model.AppUser;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
