02e04a83f133208c4d1e5dba9c4d87bd15189e5b
Jarek Styba <jarek.styba@gmail.com>
2018-11-08 18:29:39 +0100

    changed generetiontype in browsehistory to sequence

--- src/main/java/com/readbetter/main/model/LoginRecord.java
+++ src/main/java/com/readbetter/main/model/LoginRecord.java
@@ -14,7 +14,7 @@ import java.time.temporal.ChronoUnit;
 @Entity
 public class LoginRecord {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @JsonIgnore
     private Long id;
     private LocalDateTime loggedInAt;
